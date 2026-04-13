9ccab51b3da5d358b3c7dde4d000eb286dff5ba3
Sunny Kim <154963758+sskeme@users.noreply.github.com>
2026-04-13 10:13:08 -0400

    Create myMath.h

---
+++ myMath.h
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+/*TODO: write your code for the class template Absolute*/
+
+
+/*TODO: write your code for the class template Power*/
+
+
+/*TODO: complete the following code for the class template MathUtility*/
+template <typename T>
+class MathUtility {
+  /*TODO: write your code*/
+public:
+  void display() {
+    for (int i=0; i<size; i++) {
+      cout << data[i] << " ";
+    }   
+    cout << endl;
+  }
+};
